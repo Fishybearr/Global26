a15ee1f35c8a92ebd8e3aeb1643b71d7d346c12a
augie745 <95657234+augie745@users.noreply.github.com>
2026-02-01 11:58:27 -0500

    Updated PivotPoint

--- pivot_point.gd
+++ pivot_point.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
+@onready var PlayerRef : Player = $".."
+
+var MouseActive = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	MouseActive == true	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	look_at(get_global_mouse_position())
+	var joy_vector := Vector2(
+		Input.get_joy_axis(0, JOY_AXIS_RIGHT_X),
+		Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+	)
+	if (joy_vector.x*joy_vector.y!=0):
+		if (MouseActive != false):
+			MouseActive == false
+		look_at(PlayerRef.position+joy_vector)
+	elif (MouseActive == true):
+		look_at(get_global_mouse_position())
